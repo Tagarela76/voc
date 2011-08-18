c1eb4c4c6283f59f8b95005d9d4b5b4911a86f5c
took <took@a5ebc7e9-748e-d340-948a-9927c144f60a>
2011-08-18 13:40:38 +0000

    Add gauges foreach facility at company level page
    git-svn-id: http://192.168.1.2/svn/rep/trunk/voc/voc_src@8161 a5ebc7e9-748e-d340-948a-9927c144f60a

--- vwm/modules/classes/FacilityProperties.class.php
+++ vwm/modules/classes/FacilityProperties.class.php
@@ -3,39 +3,39 @@
 class FacilityProperties {
 	protected $facilityID;
 	protected $currentUsage = 0;
-	
+
 	/**
-	 * 
+	 *
 	 * wanna usage for 2008? $facility->getAnnualUsage('2008');
 	 * @var array(year=>value)
 	 */
 	protected $annualUsage = array();
-	
+
 	protected $vocLimit;
 	protected $vocAnnualLimit;
 	protected $companyID;
 
     function FacilityProperties() {
     }
-    
-    public function getDailyLimit() {
+
+    public function getMonthlyLimit() {
     	return $this->vocLimit;
     }
-    
+
     public function getCompanyID() {
     	return $this->companyID;
     }
-    
+
     public function getFacilityID() {
     	return $this->facilityID;
     }
-    
+
     public function getAnnualLimit() {
     	return $this->vocAnnualLimit;
     }
-    
+
     /**
-     * 
+     *
      * wanna usage for 2008? $facility->getAnnualUsage('2008');
      * @param string $year
      * @param string $mm month
@@ -43,11 +43,11 @@ class FacilityProperties {
      */
     public function getAnnualUsage($year, $mm = null) {
     	if ($mm === null) {
-    		return (isset($this->annualUsage[$year])) ? array_sum($this->annualUsage[$year]) : 0;	
-    	} else {    		
+    		return (isset($this->annualUsage[$year])) ? array_sum($this->annualUsage[$year]) : 0;
+    	} else {
     		return (isset($this->annualUsage[$year][$mm])) ? $this->annualUsage[$year][$mm] : 0;
     	}
-    	 
+
     }
 }
 ?>
