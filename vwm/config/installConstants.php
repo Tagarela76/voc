4d63385c32ada442a954411ddf51f6c19e3f4d4a
Dmitry Drozd <dmitry.ds@kttsoft.com>
2012-05-07 11:31:36 +0300

    gitignore

--- vwm/config/installConstants.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');      	// database host    
-define('DB_USER', 'root');   		// username         
-define('DB_PASS', 'developer');   			// password         
-define('DB_NAME', 'voc');      	// database name    
-define('REGION',  'us');			// localization		
-?>
--- vwm/template_c/supplier/%%E6^E6B^E6B6DB11%%inventoryOrders.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2012-02-15 10:43:53
-         compiled from tpls:inventory/design/inventoryOrders.tpl */ ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => '/home/developer/Workspace/voc_src/vwm/extensions/inventory/design/inventoryOrders.tpl', 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?> 
