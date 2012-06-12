b4fab3d87903c94194004431a51dff3cdcd06d2d
Denis Tukalenko <denis.nt@kttsoft.com>
2012-06-12 11:57:37 +0300

    docs for AJAXResponse class

--- vwm/modules/classes/AJAXResponse.class.php
+++ vwm/modules/classes/AJAXResponse.class.php
@@ -1,17 +1,39 @@
 <?php
 
+/**
+ * Handles server/client communication via AJAX
+ */
+
 class AJAXResponse {
-	
+
+	/**
+	 * Form validation flag
+	 * @var boolean true if validation passed, false on failure
+	 */
 	private $success = true;
+
+	/**
+	 * Flash notification text
+	 * @var string
+	 */
 	private $message = '';
+
+	/**
+	 * Array which contains errors for each form input
+	 * @var array
+	 */
 	public $validationRes = array();
-	
+
+	/**
+	 * Form data
+	 * @var mixed
+	 */
 	public $data;
-	
-	public function __construct() {		
-	}	
-	
-	
+
+	public function __construct() {
+	}
+
+
 	public function setSuccess($success) {
 		if (is_bool($success)) {
 			$this->success = $success;
@@ -19,25 +41,28 @@ class AJAXResponse {
 			return false;
 		}
 	}
-	
+
 	public function setMessage($message) {
 		if (is_string($message)) {
 			$this->message = $message;
 		} else {
 			return false;
-		}		
+		}
 	}
-	
-	
+
+
 	public function isSuccess() {
 		return $this->success;
 	}
-	
+
 	public function getMessage() {
 		return $this->message;
 	}
-	
-	
+
+
+	/**
+	 * Send AJAXResponse to client
+	 */
 	public function response() {
 		$response = array(
 			'success' => $this->isSuccess(),
@@ -47,7 +72,7 @@ class AJAXResponse {
 		);
 		echo json_encode($response);
 	}
-		
+
 }
 
 ?>
