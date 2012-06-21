3f34661beebbbf42652f545b6f049f7b9bddb3d8
Denis Tukalenko <denis.nt@kttsoft.com>
2012-06-21 19:46:39 +0300

    order PFP ratio

--- vwm/modules/classes/PFP.class.php
+++ vwm/modules/classes/PFP.class.php
@@ -7,14 +7,14 @@ class PFP
 	private $db;
 	private $id;
 	private $description;
-	
+
 	public $products;
-	
+
 	function PFP($PFPProductsArray) {
 		$this->products = $PFPProductsArray;
-		
+
 	}
-	
+
 	public function getProducts() {
 		$products = array();
 		if (isset($this->products)) {
@@ -24,50 +24,51 @@ class PFP
 				$products[] = $product;
 			}
 		}
-		
+
 		return $products;
 	}
 
 	public function getProductsCount() {
 		return isset($this->products) ? count($this->products) : "0";
 	}
-	
+
 	public function getId() {
 		return $this->id;
 	}
-	
+
 	public function setID($value){
 		$this->id = $value;
 	}
-	
+
 	public function getDescription() {
 		return $this->description;
 	}
-	
+
 	public function getRatio() {
-		
+		$res = array();
+		$j = 1;
 		$count = $this->getProductsCount();
 		for($i=0; $i<$count; $i++) {
 			if($this->products[$i]->isPrimary()){
-				$res .= "<b>".$this->products[$i]->getRatio()."</b>";
+				$res[0] = "<b>".$this->products[$i]->getRatio()."</b>";
 			} else {
-				$res .= $this->products[$i]->getRatio();
-			}
-			if($i < $count -1) {
-				$res .= ":";
+				$res[$j] = $this->products[$i]->getRatio();
+				$j++;
 			}
 		}
-		return $res;
+		ksort($res);
+		
+		return implode(':', $res);
 	}
-	
+
 	public function setDescription($val) {
 		$this->description = $val;
 	}
-	
+
 	public function save() {
-		
+
 	}
-	
+
 	public function toJson() {
 		$arr = array("descrtiption"=>$this->description, "id" => $this->id, "ratio" => $this->getRatio(), "products" => $this->products);
 		return json_encode($arr);
