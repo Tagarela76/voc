908114693ee94494788e7e984b418fcea2b71af3
Masha Andreeva <masha.an@kttsoft.com>
2012-08-16 18:48:16 +0300

    fix tests

--- vwm/tests/unit/ProductTest.php
+++ vwm/tests/unit/ProductTest.php
@@ -5,7 +5,7 @@ use VWM\Framework\Test as Testing;
 class ProductTest extends Testing\DbTestCase {
 
 	protected $fixtures = array(
-		TB_COMPANY, TB_SUPPLIER, TB_PRODUCT, TB_PFP, TB_PFP2PRODUCT, TB_PFP2COMPANY, TB_PRODUCT2PRODUCT_LIBRARY_TYPE, TB_PRODUCT_LIBRARY_TYPE
+		TB_COMPANY, TB_SUPPLIER, TB_PRODUCT, TB_PFP, TB_PFP2PRODUCT, TB_PFP2COMPANY, TB_PRODUCT_LIBRARY_TYPE, TB_PRODUCT2PRODUCT_LIBRARY_TYPE
 	);
 
 	public function testGetPFPList() {
@@ -24,7 +24,7 @@ class ProductTest extends Testing\DbTestCase {
 	public function testGetProductLibraryTypes() {
 		
 		$product = new Product($this->db);
-		$proctLibraryTypes = $product->getProductLibraryTypes('1');
+		$proctLibraryTypes = $product->getProductLibraryTypes('1'); 
 		$this->assertTrue(count($proctLibraryTypes) == 2);
 		$this->assertTrue($proctLibraryTypes[0] instanceof ProductLibraryType);
 	}
@@ -32,8 +32,9 @@ class ProductTest extends Testing\DbTestCase {
 		
 		$product = new Product($this->db);
 		$productId = 1;
-		$productLibraryTypes = array("3", "4");
-		$product->addProductLibraryTypes($productId, $productLibraryTypes);
+		$productLibraryTypes = array('3', '4');
+
+		$product->addProductLibraryTypes($productLibraryTypes, $productId);
 		
 		$proctLibraryTypes = $product->getProductLibraryTypes('1');
 		$this->assertTrue(count($proctLibraryTypes) == 4);
