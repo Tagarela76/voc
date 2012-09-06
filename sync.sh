0463047dccec8f75b4dfc61ef90386916eb6a0c1
detook <detook@margaret>
2012-09-06 12:30:55 +0300

    sync script now compresses js

--- sync.sh
+++ sync.sh
@@ -1,21 +1,39 @@
 DIRECTORY="vwm"
+RELEASE_DIRECTORY="../vwmRelease"
 
 if [ ! -d "$DIRECTORY" ]; then
 	echo "You should cd to VOCWEBMANAGER root folder before sync"
 	exit 0
 fi
 
+echo "Copying to the tmp folder vwmRelease/"
+rsync -rzvp ./$DIRECTORY/* $RELEASE_DIRECTORY/
+
+
+echo "Compressing JS..."
+JS=$RELEASE_DIRECTORY'/modules/js'
+FILELIST='minified.tmp'
+ls $JS | grep -v min > $FILELIST
+while read LINE
+do
+	if [ ! -d "$JS/$LINE" ]; then
+		FILE="$JS/$LINE"
+		java -jar tools/yuicompressor-2.4.7.jar $FILE -o $FILE
+	fi
+done < $FILELIST
+rm -f $FILELIST
+
+echo "Starting sync..."
 rsync -rzvp \
 --exclude "*.tpl.php" \
 --exclude ".svn" \
 --exclude ".git" \
---exclude "vwm/migrations/" \
---exclude "vwm/tests/" \
+--exclude "migrations/" \
+--exclude "tests/" \
 --exclude "tmp/*" \
 --exclude "config" \
---exclude "vwm/modules/phpgacl/" \
---exclude "documentation" \
---inplace ./vwm/* root@jon.vocwebmanager.com:/home/jonvo0/public_html/vocwebmanager.com/vwm/;
+--exclude "modules/phpgacl/" \
+--inplace $RELEASE_DIRECTORY/* root@jon.vocwebmanager.com:/home/jonvo0/public_html/vocwebmanager.com/vwm/;
 
 echo "Sync is done"
 exit 0
