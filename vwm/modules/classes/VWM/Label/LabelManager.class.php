7a81fd0ec2ae6235eab84b611904f4a656a261f3
Masha Andreeva <masha.an@kttsoft.com>
2012-10-18 16:02:10 +0300

    fix - if label for industry type doesn't exist we insert new label

--- vwm/modules/classes/VWM/Label/LabelManager.class.php
+++ vwm/modules/classes/VWM/Label/LabelManager.class.php
@@ -50,11 +50,28 @@ class LabelManager {
 	 * @param string $labelText
 	 */
 	public function saveRepairOrderLabel($labelText) {
-		$sql = "UPDATE " . self::TABLE_NAME . " " .
-			   "SET label_text='{$this->db->sqltext($labelText)}' " .
-			   "WHERE label_id='" . self::LABEL_ID_REPAIR_ORDER . "' " .
-               "AND industry_type_id={$this->db->sqltext($this->industryType)}";					   			
+		$sql = "SELECT * " .
+				"FROM ".self::TABLE_NAME." " .
+				"WHERE industry_type_id = {$this->db->sqltext($this->industryType)} " .
+				"AND label_id='" . self::LABEL_ID_REPAIR_ORDER . "'";		
 		$this->db->query($sql);
+		
+		if($this->db->num_rows() == 0) {
+			$sql = "INSERT INTO " . self::TABLE_NAME . " " .
+					"(industry_type_id, label_id, label_text) " .
+					"VALUES(" .
+					"{$this->db->sqltext($this->industryType)}, " .
+					"'" . self::LABEL_ID_REPAIR_ORDER . "', " .
+					"'{$this->db->sqltext($labelText)}'" .		
+					")";					   			
+			$this->db->query($sql);
+		} else {
+			$sql = "UPDATE " . self::TABLE_NAME . " " .
+					"SET label_text='{$this->db->sqltext($labelText)}' " .
+					"WHERE label_id='" . self::LABEL_ID_REPAIR_ORDER . "' " .
+					"AND industry_type_id={$this->db->sqltext($this->industryType)}";					   			
+			$this->db->query($sql);
+		}		
 	}
 }
 
