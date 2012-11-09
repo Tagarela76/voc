4e96b9840099f964a4f067c0051dedbc4853fdbe
Masha Andreeva <masha.an@kttsoft.com>
2012-11-09 08:35:06 +0200

    fix stupid mistake - forget add to migration default field - add job

--- vwm/migrations/2012-11-05.sql
+++ vwm/migrations/2012-11-05.sql
@@ -31,5 +31,5 @@ INSERT INTO  `browse_category_entity` (
 `default_value`
 )
 VALUES (
-NULL ,  'browse_category_mix',  'Product Name,Description,R/O Description,Contact,R/O VIN number,VOC,Creation Date'
+NULL ,  'browse_category_mix',  'Product Name,Add Job,Description,R/O Description,Contact,R/O VIN number,VOC,Creation Date'
 );
