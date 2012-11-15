ace97c4931a83967197c22645e6d3f8c6dcecde4
Denis Tukalenko <detook@gmail.com>
2012-11-15 20:46:38 +0200

    Need to add more characters for notes, may 200 -300 characters

--- vwm/migrations/2012-11-15.sql
+++ vwm/migrations/2012-11-15.sql
@@ -13,3 +13,5 @@ NULL , 'date', 'Last seen date', 'meeting_date', 'contacts', 'no'
 INSERT INTO `filter` (`id`, `filter_class`, `field_name`, `name_in_table`, `parent`, `autocomplete`) VALUES (NULL, 'text', 'City', 'city', 'contacts', 'no');
 
 ALTER TABLE `contacts` ADD `shop_type` SMALLINT NOT NULL DEFAULT '0';
+
+ALTER TABLE `contacts` CHANGE `comments` `comments` VARCHAR( 300 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
