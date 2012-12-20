c7c477876a1cbc9dc0323a621d516aba4c252ff2
Denis Tukalenko <detook@gmail.com>
2012-12-20 13:05:40 +0200

    fix table declaration

--- vwm/migrations/2012-12-19.sql
+++ vwm/migrations/2012-12-19.sql
@@ -1,6 +1,14 @@
-CREATE TABLE IF NOT EXISTS `pfp2department` (
+CREATE TABLE IF NOT EXISTS `pfp_type2department` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pfp_id` int(11) NOT NULL,
+  `pfp_type_id` int(11) NOT NULL,
   `department_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+ALTER TABLE  `pfp_type2department` ADD FOREIGN KEY (  `pfp_type_id` ) REFERENCES `pfp_types` (
+`id`
+) ON DELETE CASCADE ON UPDATE RESTRICT ;
+
+ALTER TABLE  `pfp_type2department` ADD FOREIGN KEY (  `department_id` ) REFERENCES  `department` (
+`department_id`
+) ON DELETE CASCADE ON UPDATE RESTRICT ;
