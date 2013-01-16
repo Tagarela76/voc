176009e5b77493d353a217b9ffe731d7a4d44268
Kucheruck <denis.kv@kttsoft.com>
2013-01-16 11:20:23 +0200

    Fix fixtures for process unit Test

--- vwm/tests/fixtures/step.php
+++ vwm/tests/fixtures/step.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-  array('number' => '1','last_update_time' => '2013-01-08','process_id' => '2','process_template_id' => NULL),
-  array('number' => '2','last_update_time' => '2013-01-10','process_id' => '2','process_template_id' => NULL),
-  array('number' => '3','last_update_time' => '2013-01-02','process_id' => '2','process_template_id' => NULL)
+  array('number' => '1','last_update_time' => '2013-01-08','process_id' => '2'),
+  array('number' => '2','last_update_time' => '2013-01-10','process_id' => '2'),
+  array('number' => '3','last_update_time' => '2013-01-02','process_id' => '2')
 );
 ?>
