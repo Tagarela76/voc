e947d7290126b31e927fc8b42f048ff057a7596d
Kucheruck <denis.kv@kttsoft.com>
2013-01-23 16:43:21 +0200

    add unittype for process

--- vwm/migrations/2013-01-23.sql
+++ vwm/migrations/2013-01-23.sql
@@ -26,3 +26,5 @@ NULL ,  'LS',  'Lump Sum', NULL ,  '7',  'USA',  '6'
 
 UPDATE  `unittype` SET  `name` =  'mins' WHERE  `unittype`.`unittype_id` =38;
 UPDATE  `unittype` SET  `name` =  'pr.' WHERE  `unittype`.`unittype_id` =36;
+UPDATE  `unittype` SET  `name` =  'ea' WHERE  `unittype`.`unittype_id` =40;
+INSERT INTO `unittype` (`name`, `unittype_desc`, `formula`, `type_id`, `system`, `unit_class_id`) VALUES ('pr click', 'pr click', NULL, 2, NULL, 1)
