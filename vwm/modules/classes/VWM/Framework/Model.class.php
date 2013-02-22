6f085bddc775c7abf1a9639b2767fd2323af3e7c
Denis Tukalenko <detook@gmail.com>
2013-02-22 11:37:03 +0200

    Cleanup Model class - PhpDoc, Formatting

--- vwm/modules/classes/VWM/Framework/Model.class.php
+++ vwm/modules/classes/VWM/Framework/Model.class.php
@@ -1,155 +1,187 @@
 <?php
 
 namespace VWM\Framework;
+
 use Symfony\Component\Validator\Validation;
 
+/**
+ * Model is the base class providing the common features needed by data model
+ * objects.
+ */
+abstract class Model
+{
 
-abstract class Model {
+    /**
+     * @var \db - xnyo databse - DEPRECATED. Use service approuch
+     * <pre>
+     * \VOCApp::getInstanse()->get('db');
+     * </pre>
+     */
+    protected $db;
 
-	/**
-	 * @var \db - xnyo databse
-	 */
-	protected $db;
+    /**
+     * @var string representation of the model. Used by validation at this moment
+     */
+    protected $modelName = "";
 
-	protected $modelName = "";
+    /**
+     * @var string validationGroup is used to trigger different validation
+     * scenarios. For example, validation rules are different for login and sign up
+     */
+    protected $validationGroup;
 
-	protected $validationGroup;
+    /**
+     * TODO: Use DateTime one day
+     * @var string common field for major models
+     */
+    protected $last_update_time;
 
-	protected $last_update_time;
+    /**
+     * @var \Symfony\Component\Validator\Validator
+     */
+    private $validator;
 
-	/**
-	 * @var \Symfony\Component\Validator\Validator;
-	 */
-	private $validator;
+    public function getLastUpdateTime()
+    {
+        return $this->last_update_time;
+    }
 
+    public function setLastUpdateTime($lastUpdateTime)
+    {
+        $this->last_update_time = $lastUpdateTime;
+    }
 
-	public function getLastUpdateTime() {
-		return $this->last_update_time;
-	}
+    /**
+     * TODO: get rid of this
+     *
+     * Overvrite get property if property is not exists or private.
+     * @param mixed $name - property name. method call method
+     * get_%property_name%, if method does not exists - return property value;
+     */
+    public function __get($name)
+    {
+        if (method_exists($this, "get_" . $name)) {
+            $methodName = "get_" . $name;
+            $res = $this->$methodName();
+            return $res;
+        } else if (property_exists($this, $name)) {
+            return $this->$name;
+        } else {
+            return false;
+        }
+    }
 
-	public function setLastUpdateTime($lastUpdateTime) {
-		$this->last_update_time = $lastUpdateTime;
-	}
+    /**
+     * TODO: get rid of this
+     *
+     * Overvrive set property. If property reload function set_%property_name%
+     * exists - call it. Else - do nothing. Keep OOP =)
+     * @param unknown_type $name - name of property
+     * @param unknown_type $value - value to set
+     */
+    public function __set($name, $value)
+    {
+        /* Call setter only if setter exists */
+        if (method_exists($this, "set_" . $name)) {
+            $methodName = "set_" . $name;
+            $this->$methodName($value);
+        }
+    }
 
-	/**
-	 *
-	 * Overvrite get property if property is not exists or private.
-	 * @param unknown_type $name - property name. method call method
-	 * get_%property_name%, if method does not exists - return property value;
-	 */
-	public function __get($name) {
-		if (method_exists($this, "get_" . $name)) {
-			$methodName = "get_" . $name;
-			$res = $this->$methodName();
-			return $res;
-		} else if (property_exists($this, $name)) {
-			return $this->$name;
-		} else {
-			return false;
-		}
-	}
+    /**
+     * @param string model name
+     *
+     * @return \Symfony\Component\Validator\Validator
+     */
+    protected function getValidator($name)
+    {
+        //	we prefer absolute path
+        if (defined('site_path')) {
+            $path = site_path . "modules" . DIRSEP . "resources" . DIRSEP . "validation" . DIRSEP;
+        } else {
+            $path = "modules" . DIRSEP . "resources" . DIRSEP . "validation" . DIRSEP;
+        }
 
-	/**
-	 *
-	 * Overvrive set property. If property reload function set_%property_name%
-	 * exists - call it. Else - do nothing. Keep OOP =)
-	 * @param unknown_type $name - name of property
-	 * @param unknown_type $value - value to set
-	 */
-	public function __set($name, $value) {
-		/* Call setter only if setter exists */
-		if (method_exists($this, "set_" . $name)) {
-			$methodName = "set_" . $name;
-			$this->$methodName($value);
-		}
-	}
+        if ($this->validator === null) {
+            $this->validator = Validation::createValidatorBuilder()
+                    ->addYamlMapping($path . $name . '.yml')
+                    ->getValidator();
+        }
 
-	/**
-	 * @param string model name
-	 * @return \Symfony\Component\Validator\Validator
-	 */
-	protected function getValidator($name) {
-		//	we prefer absolute path
-		if(defined('site_path')) {
-			$path = site_path . "modules" . DIRSEP . "resources" . DIRSEP . "validation" . DIRSEP;
-		} else {
-			$path = "modules" . DIRSEP . "resources" . DIRSEP . "validation" . DIRSEP;
-		}
+        return $this->validator;
+    }
 
-		if ($this->validator === null) {
-			$this->validator = Validation::createValidatorBuilder()
-					->addYamlMapping($path . $name . '.yml')
-					->getValidator();
-		}
+    /**
+     * @return Symfony\Component\Validator\ConstraintViolationList
+     */
+    public function validate($modelName = false)
+    {
+        if (!$modelName) {
+            $modelName = $this->modelName;
+        }
 
-		return $this->validator;
-	}
+        if ($this->validationGroup) {
+            return $this->getValidator($modelName)->validate(
+                            $this, array($this->validationGroup));
+        } else {
+            return $this->getValidator($modelName)->validate($this);
+        }
+    }
 
-	/**
-	 * @return Symfony\Component\Validator\ConstraintViolationList
-	 */
-	public function validate($modelName = false) {
-		if (!$modelName) {
-			$modelName = $this->modelName;
-		}
+    /**
+     * Ini object by properties array in key=>value format
+     *
+     * @param array $array
+     */
+    public function initByArray($array)
+    {
+        foreach ($array as $key => $value) {
+            try {
+                $this->$key = $value;
+            } catch (Exception $e) {
+                $this->errors[] = $e->getMessage();
+            }
+        }
+    }
 
-		if($this->validationGroup) {
-			return $this->getValidator($modelName)->validate(
-				$this,
-				array($this->validationGroup));
-		} else {
-			return $this->getValidator($modelName)->validate($this);
-		}
+    /**
+     * @param string
+     */
+    public function setValidationGroup($validationGroup)
+    {
+        $this->validationGroup = $validationGroup;
+    }
 
-	}
+    /**
+     * Saves model to database
+     *
+     * @return int newly created recored ID
+     */
+    public function save()
+    {
+        $this->setLastUpdateTime(date(MYSQL_DATETIME_FORMAT));
 
+        if ($this->getId()) {
+            return $this->_update();
+        } else {
+            return $this->_insert();
+        }
+    }
 
-	/**
-	 * Ini object by properties array in key=>value format
-	 * @param array $array
-	 */
-	public function initByArray($array) {
-		foreach ($array as $key => $value) {
-			try {
-				$this->$key = $value;
-			} catch (Exception $e) {
-				$this->errors[] = $e->getMessage();
-			}
-		}
-	}
+    /**
+     * Should be implemented by children
+     */
+    protected function _insert()
+    {
+        throw new \Exception("Insert should be implemented by child");
+    }
 
-	/**
-	 *
-	 * @param type string
-	 */
-	public function setValidationGroup($validationGroup) {
-		$this->validationGroup = $validationGroup;
-	}
+    /**
+     * Should be implemented by children
+     */
+    protected function _update()
+    {
+        throw new \Exception("Update should be implemented by child");
+    }
 
-	/**
-	 * Saves model to database
-	 */
-	public function save() {
-		$this->setLastUpdateTime(date(MYSQL_DATETIME_FORMAT));
-
-		if($this->getId()) {
-			return $this->_update();
-		} else {
-			return $this->_insert();
-		}
-	}
-
-	/**
-	 * Should be implemented by children
-	 */
-	protected function _insert() {
-		throw new \Exception("Insert should be implemented by child");
-	}
-
-	/**
-	 * Should be implemented by children
-	 */
-	protected function _update() {
-		throw new \Exception("Update should be implemented by child");
-	}
 }
