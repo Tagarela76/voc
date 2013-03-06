6b6c12cc1f46d5ab0cea8030e1e1e17da1fa2512
Denis Tukalenko <detook@gmail.com>
2013-03-06 16:00:08 +0200

    format DbTestCase to PSR-1

--- vwm/modules/classes/VWM/Framework/Test/DbTestCase.class.php
+++ vwm/modules/classes/VWM/Framework/Test/DbTestCase.class.php
@@ -5,69 +5,94 @@ namespace VWM\Framework\Test;
 require_once('PHPUnit/Runner/Version.php');
 require_once('PHPUnit/Autoload.php');
 
-require_once(site_path.'../vendor/phactory/lib/Phactory.php');
+require_once(site_path . '../vendor/phactory/lib/Phactory.php');
 
-abstract class DbTestCase extends TestCase {
+/**
+ * Test case which requiers data base operations
+ */
+abstract class DbTestCase extends TestCase
+{
 
-	const FIXTURE_FOLDER = '/tests/fixtures';
+    const FIXTURE_FOLDER = '/tests/fixtures';
 
-	// only instantiate pdo once for test clean-up/fixture load
-	static private $pdo = null;
+    // only instantiate pdo once for test clean-up/fixture load
+    static private $pdo = null;
 
-	//
-	protected $fixtures = false;
+    /**
+     * @var array of database tables
+     */
+    protected $fixtures = false;
 
+    public static function setUpBeforeClass()
+    {
+        if (\Phactory::getConnection() === null) {
+            if (self::$pdo == null) {
+                //TODO: use constants from constants4unittests
+                self::$pdo = new \PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
+            }
 
-	public static function setUpBeforeClass() {
-		if (\Phactory::getConnection() === null) {
-			if (self::$pdo == null) {
-				self::$pdo = new \PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
-			}
+            \Phactory::setConnection(self::$pdo);
+        }
 
-			\Phactory::setConnection(self::$pdo);
-		}
+        // reset any existing blueprints and empty any tables Phactory has used
+        \Phactory::reset();
+    }
 
-		// reset any existing blueprints and empty any tables Phactory has used
-		\Phactory::reset();
-	}
+    public static function tearDownAfterClass()
+    {
+        \Phactory::reset();
+    }
 
-	public static function tearDownAfterClass() {
-		\Phactory::reset();
-	}
+    protected function setUp()
+    {
+        if (is_array($this->fixtures)) {
+            $this->loadFixtures($this->fixtures);
+        }
+    }
 
-	protected function setUp() {
-		if (is_array($this->fixtures)) {
-			$this->loadFixtures($this->fixtures);
-		}
-	}
+    /**
+     * Load fixtures to database
+     *
+     * @param array $tableNames
+     */
+    protected function loadFixtures($tableNames)
+    {
+        //reset tables
+        \Phactory::reset();
 
-	protected function loadFixtures($tableNames) {
-		//reset tables
-		\Phactory::reset();
+        foreach ($tableNames as $tableName) {
+            //load table
+            $this->loadFixture($tableName);
+        }
+    }
 
-		foreach ($tableNames as $tableName) {
-			//load table
-			$this->loadFixture($tableName);
-		}
-	}
+    /**
+     * Load fixture to database by table name
+     *
+     * @param string $tableName
+     *
+     * @return boolean
+     */
+    protected function loadFixture($tableName)
+    {
+        $fileName = site_path . self::FIXTURE_FOLDER . DIRECTORY_SEPARATOR . $tableName . '.php';
 
+        if (!is_file($fileName)) {
 
-	protected function loadFixture($tableName) {
-		$fileName = site_path.self::FIXTURE_FOLDER.DIRECTORY_SEPARATOR.$tableName.'.php';
+            return false;
+        }
 
-		if(!is_file($fileName)) {
-			return false;
-		}
+        // https://github.com/chriskite/phactory/issues/3
+        \Phactory::setInflection($tableName, $tableName);
 
-		// https://github.com/chriskite/phactory/issues/3
-		\Phactory::setInflection($tableName, $tableName);
+        // define default values for each user we will create
+        \Phactory::define($tableName);
 
-		// define default values for each user we will create
-		\Phactory::define($tableName);
+        foreach (require($fileName) as $row) {
+            \Phactory::create($tableName, $row);
+        }
 
-		foreach(require($fileName) as $row) {
-			\Phactory::create($tableName, $row);
-		}
-	}
+        return true;
+    }
 
 }
